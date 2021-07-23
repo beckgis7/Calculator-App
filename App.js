ad6ae86e715e776d8ae4c4c4209744bbe404eff4
Evans Ofori <eofori@toldoit.com>
2021-07-23 18:10:44 +0000

    final finish

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const calc = () => {
   let num1 = Number(document.getElementById("num1").value);
   let num2 = Number(document.getElementById("num2").value);
   let sum = num1 + num2;
-  document.getElementById("sum").innerHTML  = sum;
+  // document.getElementById("sum").innerHTML  = `Your answer is ${sum}`;
+  document.getElementById("sum").value  = `Your answer is ${sum}`;
   console.log(sum);
 }
